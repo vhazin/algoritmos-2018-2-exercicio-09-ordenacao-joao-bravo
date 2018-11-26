71e882f65ca0edddafa9d3876f6a602488e42012
João Victor Montarroios <39501506+joaovictor1020@users.noreply.github.com>
2018-11-25 23:52:01 -0300

    Update heapsort.c

--- heapsort.c
+++ heapsort.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
+#include <time.h>
+#define CLOCKS_PER_SEC
 
+void heapsort(int a[], int n) {
+   int i = n / 2, pai, filho, t;
+   while(true) {
+      if (i > 0) {
+          i--;
+          t = a[i];
+      } else {
+          n--;
+          if (n == 0) return;
+          t = a[n];
+          a[n] = a[0];
+      }
+      pai = i;
+      filho = i * 2 + 1;
+      while (filho < n) {
+          if ((filho + 1 < n)  &&  (a[filho + 1] > a[filho]))
+              filho++;
+          if (a[filho] > t) {
+             a[pai] = a[filho];
+             pai = filho;
+             filho = pai * 2 + 1;
+          } else {
+             break;
+          }
+      }
+      a[pai] = t;
+   }
+}
+
+int main (void){
+    clock_t inicio, fim;
+    float tempo_final;
+    int vetor[];
+
+    printf("Digite os números que iram ser ordenados com um espaço separando cada um deles: ");
+    scanf("%d", &vetor[]);
+    inicio = clock();
+    bubbleSort();
+    fim = clock();
+    tempo_final = fim - inicio;
+    printf(tempo_final/CLOCKS_PER_SEC);
+}
