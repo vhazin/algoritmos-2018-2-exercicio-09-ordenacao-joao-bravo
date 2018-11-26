7163b423dabcd79abfa81264a4f57da25b50b82d
João Victor Montarroios <39501506+joaovictor1020@users.noreply.github.com>
2018-11-25 23:46:57 -0300

    Update bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <time.h>
+#define CLOCKS_PER_SEC
 
+void bubbleSort(int* vetor, int tamanho) {
+	int i, j, temp;
+
+	for (i = 0; i < tamanho - 1; i++){
+        for (j = (i+1); j < tamanho; j++){
+            if (vetor[j] < vetor[i]){
+                       temp = vetor[i];
+                       vetor[i] = vetor[j];
+                       vetor[j] = temp;
+                }
+            }
+    }
+}
+
+int main (void){
+    clock_t inicio, fim;
+    float tempo_final;
+    int vetor[];
+
+    printf("Digite os números que iram ser ordenados com um espaço separando cada um deles: ");
+    scanf("%d", &vetor[]);
+    inicio = clock();
+    bubbleSort();
+    fim = clock();
+    tempo_final = fim - inicio;
+    printf(tempo_final/CLOCKS_PER_SEC);
+}
